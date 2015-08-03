51b3299a5ed35dc4538bf53fa49be85c92b916f2
ecoal95 <ecoal95@gmail.com>
2015-08-03 13:57:37 +0200

    Make code PSR-2 conformant

--- src/Router/Route.php
+++ src/Router/Route.php
@@ -1,46 +1,50 @@
-<?php namespace Router;
-class Route {
-	/** The regular expresion */
-	private $expr;
-	/** The callback function */
-	private $callback;
-	/** The matches of $expr */
-	private $matches;
-	/** Allowed methods for this route */
-	private $methods = array('GET', 'POST', 'HEAD', 'PUT', 'DELETE');
+<?php
+namespace Router;
 
-	/**
-	 * Constructor
-	 * @param string $expr regular expresion to test against
-	 * @param function $callback function executed if route matches
-	 * @param string|array $methods methods allowed
-	 */
-	public function __construct($expr, $callback, $methods = null) {
-		// Allow an optional trailing backslash
-		$this->expr = '#^' . $expr . '/?$#';
-		$this->callback = $callback;
-		if( $methods !== null ) {
-			$this->methods = is_array($methods) ? $methods : array($methods);
-		}
-	}
+class Route
+{
+    /** The regular expresion */
+    private $expr;
+    /** The callback function */
+    private $callback;
+    /** The matches of $expr */
+    private $matches;
+    /** Allowed methods for this route */
+    private $methods = array('GET', 'POST', 'HEAD', 'PUT', 'DELETE');
 
-	/**
-	 * See if route matches with path
-	 * @param string $path
-	 * @return boolean
-	 */
-	public function matches($path) {
-		if( preg_match($this->expr, $path, $this->matches) && in_array($_SERVER['REQUEST_METHOD'], $this->methods) ) {
-			return true;
-		}
-		return false;
-	}
+    /**
+     * Constructor
+     * @param string $expr regular expresion to test against
+     * @param function $callback function executed if route matches
+     * @param string|array $methods methods allowed
+     */
+    public function __construct($expr, $callback, $methods = null) {
+        // Allow an optional trailing backslash
+        $this->expr = '#^' . $expr . '/?$#';
+        $this->callback = $callback;
 
-	/**
-	 * Execute the callback. The matches function needs to be called before this and return true
-	 * we don't take the first match since it's the path
-	 */
-	public function exec() {
-		return call_user_func_array($this->callback, array_slice($this->matches, 1));
-	}
+        if ($methods !== null)
+            $this->methods = is_array($methods) ? $methods : array($methods);
+    }
+
+    /**
+     * See if route matches with path
+     * @param string $path
+     * @return boolean
+     */
+    public function matches($path) {
+        if (preg_match($this->expr, $path, $this->matches) &&
+            in_array($_SERVER['REQUEST_METHOD'], $this->methods))
+            return true;
+
+        return false;
+    }
+
+    /**
+     * Execute the callback. The matches function needs to be called before this and return true
+     * we don't take the first match since it's the path
+     */
+    public function exec() {
+        return call_user_func_array($this->callback, array_slice($this->matches, 1));
+    }
 }
--- src/Router/Router.php
+++ src/Router/Router.php
@@ -1,117 +1,116 @@
-<?php namespace Router;
-class Router {
-	/** Base url */
-	private $base_path;
-	/** Current relative url */
-	private $path;
-	/**
-	 * @var Route[]
-	 */
-	public $routes = array();
+<?php
+namespace Router;
 
-	/**
-	 * Constructor
-	 * @param string $base_path the index url
-	 */
-	public function __construct($base_path = '') {
-		$this->base_path = $base_path;
-		$path = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-		$path = substr($path, strlen($base_path));
-		$this->path = $path;
-	}
+class Router
+{
+    /** Base url */
+    private $base_path;
 
-	/**
-	 * Add a route
-	 * @param string $expr
-	 * @param mixed $callback
-	 * @param array|string $methods
-	 * @return void
-	 */
-	public function all($expr, $callback, $methods = null) {
-		$this->routes[] = new Route($expr, $callback, $methods);
-	}
+    /** Current relative url */
+    private $path;
 
-	/**
-	 * Alias for all
-	 * @param string $expr
-	 * @param mixed $callback
-	 * @param null|array $methods
-	 */
-	public function add($expr, $callback, $methods = null) {
-		$this->all($expr, $callback, $methods);
-	}
+    /**
+     * @var Route[]
+     */
+    public $routes = array();
 
-	/**
-	 * Add a route for GET requests
-	 * @param string $expr
-	 * @param mixed $callback
-	 */
-	public function get($expr, $callback) {
-		$this->routes[] = new Route($expr, $callback, 'GET');
-	}
+    /**
+     * Constructor
+     * @param string $base_path the index url
+     */
+    public function __construct($base_path = '') {
+        $this->base_path = $base_path;
+        $path = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+        $path = substr($path, strlen($base_path));
+        $this->path = $path;
+    }
 
-	/**
-	 * Add a route for POST requests
-	 * @param string $expr
-	 * @param mixed $callback
-	 */
-	public function post($expr, $callback) {
-		$this->routes[] = new Route($expr, $callback, 'POST');
-	}
+    /**
+     * Add a route
+     * @param string $expr
+     * @param mixed $callback
+     * @param array|string $methods
+     * @return void
+     */
+    public function all($expr, $callback, $methods = null) {
+        $this->routes[] = new Route($expr, $callback, $methods);
+    }
 
-	/**
-	 * Add a route for HEAD requests
-	 * @param string $expr
-	 * @param mixed $callback
-	 */
-	public function head($expr, $callback) {
-		$this->routes[] = new Route($expr, $callback, 'HEAD');
-	}
+    /**
+     * Alias for all
+     * @param string $expr
+     * @param mixed $callback
+     * @param null|array $methods
+     */
+    public function add($expr, $callback, $methods = null) {
+        $this->all($expr, $callback, $methods);
+    }
 
-	/**
-	 * Add a route for PUT requests
-	 * @param string $expr
-	 * @param mixed $callback
-	 */
-	public function put($expr, $callback) {
-		$this->routes[] = new Route($expr, $callback, 'PUT');
-	}
+    /**
+     * Add a route for GET requests
+     * @param string $expr
+     * @param mixed $callback
+     */
+    public function get($expr, $callback) {
+        $this->routes[] = new Route($expr, $callback, 'GET');
+    }
 
-	/**
-	 * Add a route for DELETE requests
-	 * @param string $expr
-	 * @param mixed $callback
-	 */
-	public function delete($expr, $callback) {
-		$this->routes[] = new Route($expr, $callback, 'DELETE');
-	}
+    /**
+     * Add a route for POST requests
+     * @param string $expr
+     * @param mixed $callback
+     */
+    public function post($expr, $callback) {
+        $this->routes[] = new Route($expr, $callback, 'POST');
+    }
 
-	/**
-	 * Test all routes until any of them matches
-	 */
-	public function route() {
-		foreach($this->routes as $route)
-		{
-			if($route->matches($this->path))
-			{
-				return $route->exec();
-			}
-		}
+    /**
+     * Add a route for HEAD requests
+     * @param string $expr
+     * @param mixed $callback
+     */
+    public function head($expr, $callback) {
+        $this->routes[] = new Route($expr, $callback, 'HEAD');
+    }
 
-		throw new \Exception("No routes matching {$this->path}");
-	}
+    /**
+     * Add a route for PUT requests
+     * @param string $expr
+     * @param mixed $callback
+     */
+    public function put($expr, $callback) {
+        $this->routes[] = new Route($expr, $callback, 'PUT');
+    }
 
-	/**
-	 * Get the current url or the url to a path
-	 * @param string $path
-	 * @return string
-	 */
-	public function url($path = null) {
-		if($path === null)
-		{
-			$path = $this->path;
-		}
+    /**
+     * Add a route for DELETE requests
+     * @param string $expr
+     * @param mixed $callback
+     */
+    public function delete($expr, $callback) {
+        $this->routes[] = new Route($expr, $callback, 'DELETE');
+    }
 
-		return $this->base_path . $path;
-	}
+    /**
+     * Test all routes until any of them matches
+     */
+    public function route() {
+        foreach ($this->routes as $route)
+            if ($route->matches($this->path))
+                return $route->exec();
+
+        throw new \Exception("No routes matching {$this->path}");
+    }
+
+    /**
+     * Get the current url or the url to a path
+     * @param string $path
+     * @return string
+     */
+    public function url($path = null) {
+        if ($path === null)
+            $path = $this->path;
+
+        return $this->base_path . $path;
+    }
 }
